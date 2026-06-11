ca410c8e546d564f5eda0ca1cccc46f8e5e8d531
qutbro <129995068+Qutbro@users.noreply.github.com>
2026-06-12 02:42:42 +0300

    Add initial configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+API_TON = ""
+
+MNEMONIC = [
+
+]
+
+DATA = {
+    'stel_ssid': '',
+    'stel_dt': '',
+    'stel_ton_token': '',
+    'stel_token': '',
+}
+
+FRAGMENT_HASH = ''
+FRAGMENT_PUBLICKEY = ''
+FRAGMENT_WALLETS = ''
+FRAGMENT_ADDRES = ''
